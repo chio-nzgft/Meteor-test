31277483edf09e735209a1592979eadb76102b56
echochio <chio@nzgft.com>
2017-02-03 12:13:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 if (Meteor.isClient){
-        Template.date_display.helpers({
-                current_date:function(){
-                        return new Date();
-                }
-        });
+	// uodate session variable very 1000 ms
+	Meteor.setInterval(function(){
+		Session.set("current_date", new Date());
+	}, 1000);
+	// display session var
+	Template.date_display.helpers({
+		current_date:function(){
+			return Session.get("current_date");
+		}
+	});
 }
 
 if (Meteor.isServer){
-        Meteor.startup(function(){
-                // code to run on server at startup
-        });
+	Meteor.startup(function(){
+		// code to run on server at startup
+	});
 }
